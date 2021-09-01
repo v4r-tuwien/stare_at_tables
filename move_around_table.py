8957a3f50ba55dead7b7145fcd61ff7233863533
Markus Leitner <leitnermrks@gmail.com>
2021-09-01 11:40:16 +0200

    use PoseStamped directly, adapt lift to height, record at first pose

--- move_around_table.py
+++ move_around_table.py
@@ -1,38 +1,53 @@
 import actionlib
 import rospy
-import tf
-import tf.transformations
-from actionlib_msgs.msg import GoalStatus
-from geometry_msgs.msg import Point, PoseStamped, Quaternion
 from hsrb_interface import Robot
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from std_srvs.srv import Empty, EmptyRequest
 import numpy as np
 import subprocess
+from tf.transformations import quaternion_about_axis, quaternion_multiply
 
-def fill_goal(goal_x, goal_y, goal_yaw):
-	# fill ROS message	
-	pose = PoseStamped()
-	pose.header.stamp = rospy.Time.now()
-	pose.header.frame_id = "map"
-	pose.pose.position = Point(goal_x, goal_y, 0)
-	quat = tf.transformations.quaternion_from_euler(0, 0, goal_yaw)
-	pose.pose.orientation = Quaternion(*quat)
-
-	goal = MoveBaseGoal()
-	goal.target_pose = pose
-	return goal
+from table_extractor.msg import Table
+from mongodb_store.message_store import MessageStoreProxy
 
 robot = Robot()
 whole_body = robot.get('whole_body')
 base = robot.get('omni_base')
 
-whole_body.move_to_joint_positions({'head_tilt_joint': -1.0})
-whole_body.gaze_point(point=[0.68, 0 ,0.45], ref_frame_id='map')
-
-poses = np.loadtxt('sasha_lab.txt', delimiter=',')
 
 
+msg_store = MessageStoreProxy()
+poses = []
+
+##TODO select the plane
+for msg, meta in msg_store.query(Table._type):
+	if msg.id==1:
+		poses = msg.viewposes
+		for pose in poses:
+			q = [pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w]
+			q_rotate = quaternion_about_axis(np.pi/2, (0,0,1))
+			q = quaternion_multiply(q_rotate, q)
+			pose.pose.orientation.x = q[0]
+			pose.pose.orientation.y = q[1]
+			pose.pose.orientation.z = q[2]
+			pose.pose.orientation.w = q[3]
+		gaze_point = np.asarray([msg.center.point.x, msg.center.point.y, msg.center.point.z])
+		height = msg.height
+poses = poses[::-1]
+
+whole_body.move_to_neutral()
+if height>0.4:
+	whole_body.move_to_joint_positions({'arm_lift_joint': height-0.4})
+whole_body.move_to_joint_positions({'arm_flex_joint': -0.2,
+            'arm_roll_joint': 1.570,
+            'hand_motor_joint': 1.0,
+            'head_pan_joint': 0.0,
+            'head_tilt_joint': -0.75,
+            'wrist_flex_joint': -1.57,
+            'wrist_roll_joint': 0.0})
+whole_body.gaze_point(point=gaze_point, ref_frame_id='map')
+
+print(poses)
 stop_client = rospy.ServiceProxy('/viewpoint_controller/stop', Empty)
 stop_client.call(EmptyRequest())
 
@@ -42,19 +57,29 @@ move_client = actionlib.SimpleActionClient('/move_base/move', MoveBaseAction)
 move_client.wait_for_server()
 
 # start rosbag recording
-print("Start recording rosbag file...")
 cmd_rosbag = ['rosbag', 'record','-b','0','/hsrb/head_rgbd_sensor/rgb/image_raw', '/hsrb/head_rgbd_sensor/depth_registered/image_raw','/tf','/tf_static','/hsrb/head_rgbd_sensor/rgb/camera_info',"__name:=my_bag"]
-rosbag = subprocess.Popen(cmd_rosbag,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+rosbagflag = False
 # move to poses
 for pose in poses:
 	print(pose)
-	goal = fill_goal(*pose)
+	goal = MoveBaseGoal()
+	goal.target_pose = pose
 	move_client.send_goal(goal)
+    print('pose goal sent')
+    whole_body.gaze_point(point=gaze_point, ref_frame_id='map')
+    rospy.sleep(0.5)
 	while True:
-		whole_body.gaze_point(point=[0.68, 0 ,0.45], ref_frame_id='map')
-		print('gaze in loop')
+		whole_body.gaze_point(point=gaze_point, ref_frame_id='map')
+		rospy.sleep(0.5)
+		#print('gaze in loop')
+		#print(move_client.get_goal_status_text())
+		#print(move_client.get_state())
 		if move_client.get_state()>1:
+			if move_client.get_state()==3 and not rosbagflag:
+				rosbag = subprocess.Popen(cmd_rosbag,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				print("Start recording rosbag file...")
+				rosbagflag = True
+				rospy.sleep(0.5)
 			break
 
 # kill rosbag
